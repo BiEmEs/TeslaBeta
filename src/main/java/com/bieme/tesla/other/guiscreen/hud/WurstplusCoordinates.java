3f67f6b384cdba73eb52aa1e8f525a1b957446ad
BiEmEs <mikeila.otve1@ztudy.edu.co>
2025-07-28 23:53:49 -0500

    Coordinates.java

--- src/main/java/com/bieme/tesla/other/guiscreen/hud/WurstplusCoordinates.java
+++ src/main/java/com/bieme/tesla/other/guiscreen/hud/WurstplusCoordinates.java
@@ -1,38 +1,48 @@
 package com.bieme.tesla.other.guiscreen.hud;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
-import me.travis.wurstplus.Wurstplus;
+import com.bieme.tesla.Client;
 import com.bieme.tesla.other.guiscreen.render.pinnables.Pinnable;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 
+public class Coordinates extends Pinnable {
 
-public class WurstplusCoordinates extends Pinnable {
-	ChatFormatting dg = ChatFormatting.DARK_GRAY;
-	ChatFormatting db = ChatFormatting.DARK_BLUE;
-	ChatFormatting dr = ChatFormatting.DARK_RED;
-
-	public WurstplusCoordinates() {
+	public Coordinates() {
 		super("Coordinates", "Coordinates", 1, 0, 0);
 	}
 
 	@Override
-	public void render() {
-		int nl_r = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorR").get_value(1);
-		int nl_g = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorG").get_value(1);
-		int nl_b = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorB").get_value(1);
-		int nl_a = Wurstplus.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorA").get_value(1);
+	public void render(GuiGraphics guiGraphics) {
+		if (mc.player == null) return;
 
-		String x = Wurstplus.g + "[" + Wurstplus.r + Integer.toString((int) (mc.player.posX)) + Wurstplus.g + "]" + Wurstplus.r;
-		String y = Wurstplus.g + "[" + Wurstplus.r + Integer.toString((int) (mc.player.posY)) + Wurstplus.g + "]" + Wurstplus.r;
-		String z = Wurstplus.g + "[" + Wurstplus.r + Integer.toString((int) (mc.player.posZ)) + Wurstplus.g + "]" + Wurstplus.r;
+		int nl_r = Client.getSettingManager().getSettingByTag("HUDStringsColorR").getSliderValueInt();
+		int nl_g = Client.getSettingManager().getSettingByTag("HUDStringsColorG").getSliderValueInt();
+		int nl_b = Client.getSettingManager().getSettingByTag("HUDStringsColorB").getSliderValueInt();
+		int nl_a = Client.getSettingManager().getSettingByTag("HUDStringsColorA").getSliderValueInt();
 
-		String x_nether = Wurstplus.g + "[" + Wurstplus.r + Long.toString(Math.round(mc.player.dimension != -1 ? (mc.player.posX / 8) : (mc.player.posX * 8))) + Wurstplus.g + "]" + Wurstplus.r;
-		String z_nether = Wurstplus.g + "[" + Wurstplus.r + Long.toString(Math.round(mc.player.dimension != -1 ? (mc.player.posZ / 8) : (mc.player.posZ * 8))) + Wurstplus.g + "]" + Wurstplus.r;
+		int posX = (int) mc.player.getX();
+		int posY = (int) mc.player.getY();
+		int posZ = (int) mc.player.getZ();
+		
+		ResourceLocation dimension = mc.player.level().dimension().location();
+		boolean inNether = dimension.toString().contains("nether");
 
-		String line = "XYZ " + x + y + z + " XZ " + x_nether + z_nether;
+		int netherX = inNether ? posX * 8 : posX / 8;
+		int netherZ = inNether ? posZ * 8 : posZ / 8;
 
-		create_line(line, this.docking(1, line), 2, nl_r, nl_g, nl_b, nl_a);
+		String line =
+				Client.g + "XYZ " +
+						Client.r + "[" + posX + "] " +
+						Client.r + "[" + posY + "] " +
+						Client.r + "[" + posZ + "] " +
+						Client.g + " | " +
+						Client.g + "Nether XZ " +
+						Client.r + "[" + netherX + "] " +
+						Client.r + "[" + netherZ + "]";
 
-		this.set_width(this.get(line, "width"));
-		this.set_height(this.get(line, "height") + 2);
+		create_line(line, this.docking(2, line), 2, nl_r, nl_g, nl_b, nl_a);
+
+		this.set_width(this.get(line, "width") + 4);
+		this.set_height(this.get(line, "height") + 4);
 	}
 }
