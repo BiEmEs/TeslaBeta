5f7168f2321863dc2797f5a498401f830b06e6ab
BiEmEs <mikeila.otve1@ztudy.edu.co>
2025-08-09 11:39:52 -0500

    Update Slider.java

--- src/main/java/com/bieme/tesla/other/guiscreen/render/components/widgets/Slider.java
+++ src/main/java/com/bieme/tesla/other/guiscreen/render/components/widgets/Slider.java
@@ -3,77 +3,88 @@ package com.bieme.tesla.other.guiscreen.render.components.widgets;
 import com.bieme.tesla.other.guiscreen.settings.Setting;
 import com.bieme.tesla.Client;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
 
 public class Slider {
-	private final Setting setting;
-	private int x, y, width, height;
-	private boolean dragging = false;
-	private double percent = 0.0;
+    private final Setting setting;
+    private int x, y, width, height;
+    private boolean dragging = false;
+    private double percent = 0.0;
 
-	public int save_y;
-	private final Minecraft mc = Minecraft.getInstance();
+    public int save_y;
+    private final Minecraft mc = Minecraft.getInstance();
 
-	public Slider(Setting setting, int x, int y, int width, int height) {
-		this.setting = setting;
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+    public Slider(Setting setting, int x, int y, int width, int height) {
+        this.setting = setting;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
 
-		double min = setting.getMin();
-		double max = setting.getMax();
-		double value = setting.getValue();
-		this.percent = (value - min) / (max - min);
-	}
+        double min = setting.getMin();
+        double max = setting.getMax();
+        double value = setting.getValue();
+        this.percent = (value - min) / (max - min);
+    }
 
-	public void render(GuiGraphics guiGraphics, int masterY, int mouseX, int mouseY) {
-		this.save_y = this.y + masterY;
+    public void render(GuiGraphics guiGraphics, int masterY, int mouseX, int mouseY) {
+        this.save_y = this.y + masterY;
 
-		int bgColor = Client.getInstance().getClickGui().getBackgroundColor();
-		int fillColor = Client.getInstance().getClickGui().getSliderFillColor();
-		int textColor = Client.getInstance().getClickGui().getTextColor();
+        int bgColor = Client.getInstance().getClickGui().getBackgroundColor();
+        int fillColor = Client.getInstance().getClickGui().getSliderFillColor();
+        int textColor = Client.getInstance().getClickGui().getTextColor();
 
-		guiGraphics.fill(x, save_y, x + width, save_y + height, bgColor);
+        guiGraphics.fill(x, save_y, x + width, save_y + height, bgColor);
 
-		int filledWidth = (int) (percent * width);
-		guiGraphics.fill(x, save_y, x + filledWidth, save_y + height, fillColor);
+        int filledWidth = (int) (percent * width);
+        guiGraphics.fill(x, save_y, x + filledWidth, save_y + height, fillColor);
 
-		Font font = mc.font;
-		String label = setting.getName() + ": " + String.format("%.2f", setting.getValue());
-		guiGraphics.drawString(font, label, x + 4, save_y + 2, textColor, false);
+        String valueText = setting.getName() + ": " + String.format("%.2f", setting.getValue());
+        guiGraphics.drawString(mc.font, Component.literal(valueText), x + 4, save_y + 2, textColor);
 
-		if (dragging) {
-			updateSlider(mouseX);
-		}
-	}
+        if (dragging) {
+            updateSlider(mouseX);
+        }
+    }
 
-	public void mouseClicked(int mouseX, int mouseY, int button) {
-		if (button == 0 && isHovered(mouseX, mouseY)) {
-			dragging = true;
-			updateSlider(mouseX);
-		}
-	}
+    public void mouseClicked(int mouseX, int mouseY, int button) {
+        if (button == 0 && isHovered(mouseX, mouseY)) {
+            dragging = true;
+            updateSlider(mouseX);
+        }
+    }
 
-	public void mouseReleased(int button) {
-		if (button == 0) {
-			dragging = false;
-		}
-	}
+    public void mouseReleased(int button) {
+        if (button == 0) {
+            dragging = false;
+        }
+    }
 
-	private void updateSlider(int mouseX) {
-		percent = (mouseX - x) / (double) width;
-		percent = Math.max(0.0, Math.min(1.0, percent));
+    public void mouseDragged(int mouseX, int mouseY, int button) {
+        if (dragging && button == 0) {
+            updateSlider(mouseX);
+        }
+    }
 
-		double min = setting.getMin();
-		double max = setting.getMax();
-		double newValue = min + percent * (max - min);
-		setting.setValue(newValue);
-	}
+    private void updateSlider(int mouseX) {
+        percent = (mouseX - x) / (double) width;
+        percent = Math.max(0.0, Math.min(1.0, percent));
 
-	private boolean isHovered(int mouseX, int mouseY) {
-		return mouseX >= x && mouseX <= x + width &&
-				mouseY >= save_y && mouseY <= save_y + height;
-	}
+        double min = setting.getMin();
+        double max = setting.getMax();
+        double newValue = min + percent * (max - min);
+
+        if (setting.isInteger()) {
+            newValue = Math.round(newValue);
+        }
+
+        setting.setValue(newValue);
+    }
+
+    private boolean isHovered(int mouseX, int mouseY) {
+        return mouseX >= x && mouseX <= x + width &&
+               mouseY >= save_y && mouseY <= save_y + height;
+    }
 }
+
