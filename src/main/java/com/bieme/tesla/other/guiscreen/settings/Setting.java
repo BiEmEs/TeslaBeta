3f0b8114bf6bce2d3f990921c11633cbb2e9fb09
BiEmEs <mikeila.otve1@ztudy.edu.co>
2025-08-09 11:12:48 -0500

    Update Setting.java

--- src/main/java/com/bieme/tesla/other/guiscreen/settings/Setting.java
+++ src/main/java/com/bieme/tesla/other/guiscreen/settings/Setting.java
@@ -1,13 +1,8 @@
 package com.bieme.tesla.other.guiscreen.settings;
 
 import com.bieme.tesla.modules.hacks.Module;
-
 import java.util.List;
 
-/**
- * Clase que representa una configuración (Setting) de un módulo,
- * puede ser boolean, slider, combobox o label.
- */
 public class Setting {
 
 	private final Module master;
@@ -24,11 +19,7 @@ public class Setting {
 	private String stringValue;
 
 	private final String type;
-
-	// Visibilidad, por defecto visible
 	private boolean visible = true;
-
-	// Valor por defecto para reset
 	private final Object defaultValue;
 
 	// Boolean setting
@@ -62,7 +53,6 @@ public class Setting {
 		this.defaultValue = value;
 	}
 
-	// Double slider setting
 	public Setting(Module master, String name, String tag, double value, double min, double max) {
 		this.master = master;
 		this.name = name;
@@ -74,7 +64,6 @@ public class Setting {
 		this.defaultValue = value;
 	}
 
-	// Integer slider setting
 	public Setting(Module master, String name, String tag, int value, int min, int max) {
 		this.master = master;
 		this.name = name;
@@ -86,122 +75,46 @@ public class Setting {
 		this.defaultValue = value;
 	}
 
-	public Module getMaster() {
-		return master;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getTag() {
-		return tag;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	// Boolean
-	public boolean getBoolValue() {
-		return boolValue;
-	}
-
-	public void setBoolValue(boolean value) {
-		this.boolValue = value;
-	}
-
-	// Combobox
-	public List<String> getComboBoxValues() {
-		return comboBoxValues;
-	}
-
-	public String getCurrentValue() {
-		return currentValue;
-	}
-
+	// Métodos originales
+	public Module getMaster() { return master; }
+	public String getName() { return name; }
+	public String getTag() { return tag; }
+	public String getType() { return type; }
+	public boolean getBoolValue() { return boolValue; }
+	public void setBoolValue(boolean value) { this.boolValue = value; }
+	public List<String> getComboBoxValues() { return comboBoxValues; }
+	public String getCurrentValue() { return currentValue; }
 	public void setCurrentValue(String value) {
 		if (comboBoxValues != null && comboBoxValues.contains(value)) {
 			this.currentValue = value;
 		}
 	}
+	public boolean in(String value) { return currentValue.equalsIgnoreCase(value); }
+	public String getStringValue() { return stringValue; }
+	public void setStringValue(String value) { this.stringValue = value; }
+	public double getSliderValue() { return sliderValue; }
+	public int getSliderValueInt() { return (int) Math.round(sliderValue); }
+	public void setSliderValue(double value) { this.sliderValue = clamp(value, min, max); }
+	public void setSliderValue(int value) { this.sliderValue = clamp(value, min, max); }
+	public double getMin() { return min; }
+	public double getMax() { return max; }
+	public int getMinInt() { return (int) min; }
+	public int getMaxInt() { return (int) max; }
 
-	public boolean in(String value) {
-		return currentValue.equalsIgnoreCase(value);
-	}
-
-	// Label
-	public String getStringValue() {
-		return stringValue;
-	}
-
-	public void setStringValue(String value) {
-		this.stringValue = value;
-	}
-
-	// Slider (double/int)
-	public double getSliderValue() {
-		return sliderValue;
-	}
-
-	public int getSliderValueInt() {
-		return (int) Math.round(sliderValue);
-	}
-
-	public void setSliderValue(double value) {
-		this.sliderValue = clamp(value, min, max);
-	}
-
-	public void setSliderValue(int value) {
-		this.sliderValue = clamp(value, min, max);
-	}
-
-	public double getMin() {
-		return min;
-	}
-
-	public double getMax() {
-		return max;
-	}
-
-	public int getMinInt() {
-		return (int) min;
-	}
-
-	public int getMaxInt() {
-		return (int) max;
-	}
-
-	/** Clamp value between min and max. */
 	private double clamp(double val, double min, double max) {
 		return Math.max(min, Math.min(max, val));
 	}
 
-	/** Si quieres controlar visibilidad del setting. */
-	public boolean isVisible() {
-		return visible;
-	}
+	public boolean isVisible() { return visible; }
+	public void setVisible(boolean visible) { this.visible = visible; }
 
-	public void setVisible(boolean visible) {
-		this.visible = visible;
-	}
-
-	/** Resetea el valor al valor por defecto. */
 	public void resetToDefault() {
 		switch (type) {
-			case "boolean":
-				this.boolValue = (boolean) defaultValue;
-				break;
-			case "combobox":
-				this.currentValue = (String) defaultValue;
-				break;
-			case "label":
-				this.stringValue = (String) defaultValue;
-				break;
+			case "boolean": this.boolValue = (boolean) defaultValue; break;
+			case "combobox": this.currentValue = (String) defaultValue; break;
+			case "label": this.stringValue = (String) defaultValue; break;
 			case "double":
-			case "int":
-				this.sliderValue = ((Number) defaultValue).doubleValue();
-				break;
+			case "int": this.sliderValue = ((Number) defaultValue).doubleValue(); break;
 		}
 	}
 
@@ -225,5 +138,7 @@ public class Setting {
 			default: return "";
 		}
 	}
-
+	
+	public Module get_parent_module() { return master; }
+	public String get_tag() { return tag; }
 }
