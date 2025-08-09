ab8c99ab7e68500de8e61f8c6251ecd736c7a67f
BiEmEs <mikeila.otve1@ztudy.edu.co>
2025-08-09 11:14:42 -0500

    Update Module.java

--- src/main/java/com/bieme/tesla/modules/hacks/Module.java
+++ src/main/java/com/bieme/tesla/modules/hacks/Module.java
@@ -3,7 +3,6 @@ package com.bieme.tesla.modules.hacks;
 import com.bieme.tesla.Client;
 import com.bieme.tesla.modules.event.EventBus;
 import com.bieme.tesla.other.guiscreen.settings.Setting;
-
 import net.minecraft.client.Minecraft;
 
 import java.util.ArrayList;
@@ -18,7 +17,7 @@ public abstract class Module {
     private final String description;
     private final Category category;
 
-    private int bind = -1;
+    private int bind;
     private boolean enabled = false;
     private boolean sendToggleMessage = true;
 
@@ -27,8 +26,10 @@ public abstract class Module {
         this.tag = tag;
         this.description = description;
         this.category = category;
+        this.setBind(0);
     }
 
+    // Getters
     public String getName() { return name; }
     public String getTag() { return tag; }
     public String getDescription() { return description; }
@@ -38,9 +39,14 @@ public abstract class Module {
     public int getBind() { return bind; }
     public boolean canSendToggleMessage() { return sendToggleMessage; }
 
+    public String get_name() { return this.name; }
+    public String get_tag() { return this.tag; }
+
+    // Setters
     public void setBind(int key) { this.bind = key; }
     public void setSendToggleMessage(boolean value) { this.sendToggleMessage = value; }
 
+    // Toggle
     public void toggle() {
         setEnabled(!enabled);
     }
@@ -61,6 +67,7 @@ public abstract class Module {
         }
     }
 
+    // Settings creation
     protected Setting create(String name, String tag, int value, int min, int max) {
         Setting setting = new Setting(this, name, tag, value, min, max);
         Client.getSettingManager().register(this, setting);
@@ -95,6 +102,7 @@ public abstract class Module {
         return new ArrayList<>(Arrays.asList(items));
     }
 
+    // Hooks
     public void onEnable() {}
     public void onDisable() {}
     public void onUpdate() {}
